5dc43681890cd9c35dde3498e68aabafba505f22
ChaimEvans <xmldxlq@outlook.com>
2025-09-15 14:36:17 +0800

    项目初始化

---
+++ src/main/java/fun/chaim/DFTE/DistriFlowTaskEngineApplication.java
@@ -0,0 +1,13 @@
+package fun.chaim.DFTE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistriFlowTaskEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistriFlowTaskEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fun/chaim/DFTE/DistriFlowTaskEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package fun.chaim.DFTE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistriFlowTaskEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
